b6cef1d2fc6edd26fe946129a0f962765ea16153
Dev N <devpaduthol@gmail.com>
2021-11-08 22:21:30 -0600

    Create final_project.py
    Started with a description of the project

---
+++ final_project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Dev Nambudiripad
+"""
+
+import streamlit as st
+
+
+st.title("Stock Portfolio Optimization")
+st.write("This application would allow an individual to enter up to five stock tickers and a total amount to invest"
+         " as well as a time period. Then this application would use a predictive model to" " "
+         "first predict the closing price of each of the tickers and then provide an optimal share" " "
+         "allocation to maximize returns")
+
+
+
+
+
+
+
+
